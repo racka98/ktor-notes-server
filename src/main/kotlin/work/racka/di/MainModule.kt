343dfe6ab3f5d69249a86210534198da7c36a3f4
racka98 <bmkilaha@gmail.com>
2022-02-17 19:32:25 +0300

    fixes

--- src/main/kotlin/work/racka/di/MainModule.kt
+++ src/main/kotlin/work/racka/di/MainModule.kt
@@ -14,18 +14,29 @@ val mainModule = module {
     single {
         val config = HikariConfig()
         config.apply {
-            val uri = URI(System.getenv("ONLINE_DATABASE_URL"))
-            val userInfo = uri.userInfo.split(":").toTypedArray()
-            val username = userInfo[0]
-            val password = userInfo[1]
-
             driverClassName = System.getenv("JDBC_DRIVER")
-            //jdbcUrl = System.getenv("DATABASE_URL") For Local Use
-            jdbcUrl =
-                "jdbc:postgresql://${uri.host}:${uri.port}${uri.path}?sslmode=require&user=$username&password=$password"
             maximumPoolSize = 3
             isAutoCommit = false
             transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+
+            /**
+             * This has been wrapped inside a try block so that we can test with
+             * our local Postgres database using our local environment variable
+             * Since the URLs don't match it will throw an exception on the local
+             * machine and will use the local environment variable but will not throw
+             * an exception in our remote server since the URL will be correct for try { }
+             */
+            jdbcUrl = try {
+                val uri = URI(System.getenv("DATABASE_URL"))
+                val userInfo = uri.userInfo.split(":").toTypedArray()
+                val username = userInfo[0]
+                val password = userInfo[1]
+                "jdbc:postgresql://${uri.host}:${uri.port}${uri.path}?sslmode=require&user=$username&password=$password"
+            } catch (e: Exception) {
+                e.printStackTrace()
+                System.getenv("DATABASE_URL")
+            }
+
             validate()
         }
         HikariDataSource(config)
